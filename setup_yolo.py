98ae2bddbeba564753be9fcf01a02dc30e6b77fd
ilovax <tasfaoutabderrahimilovax@gmail.com>
2020-10-02 22:29:17 +0100

    frame distances detection and output creation

--- setup_yolo.py
+++ setup_yolo.py
@@ -15,3 +15,27 @@ def Setup(yolo_path,cv2):
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     return net, ln, labels
 
+def detect_and_write(yolo,opname,cap,frameno,create,cv2):
+	while(True):
+		#get next frame
+		
+	    ret, frame = cap.read()
+	    if not ret:
+	        break
+	    current_img = frame.copy()
+	    current_img = imutils.resize(current_img, width=480)
+	    video = current_img.shape
+	    frameno += 1
+	    if(frameno%2 == 0 or frameno == 1):
+	        net, ln, labels = Setup(yolo,cv2)
+	        processedImg = ImageProcess(current_img,net,ln,labels,cv2)
+	        Frame = processedImg
+	        #show the modified frame
+	        cv2.imshow("Image", Frame)
+	        if create is None:
+	            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+	            create = cv2.VideoWriter(opname, fourcc, 30, (Frame.shape[1], Frame.shape[0]), True)
+	    #write frame to output video
+	    create.write(Frame)
+	    if cv2.waitKey(1) & 0xFF == ord('s'):
+	        break
