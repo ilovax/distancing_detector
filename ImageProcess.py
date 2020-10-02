e1167d1dcab358a377bd46982074c618d850d831
ilovax <tasfaoutabderrahimilovax@gmail.com>
2020-10-02 22:31:21 +0100

    images precessing and checking distance

---
+++ ImageProcess.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+def Check_distance(a,  b):
+    dist = ((a[0] - b[0]) ** 2 + 550 / ((a[1] + b[1]) / 2) * (a[1] - b[1]) ** 2) ** 0.5
+    calibration = (a[1] + b[1]) / 2      
+    if 0 < dist < 0.25 * calibration:
+        return True
+    else:
+        return False
+
+def ImageProcess(image,net,ln,LABELS,cv2):
+    print("procssing a frame")
+    #take a single frame of video and then process it for social distancing detection
+    (H, W) = (None, None)
+    frame = image.copy()
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+    confidences = []
+    outline = []
+    for output in layerOutputs:
+        for detection in output:
+            #object detection
+            scores = detection[5:]
+            maxi_class = np.argmax(scores)
+            confidence = scores[maxi_class]
+            #if object is person get the x and y
+            if LABELS[maxi_class] == "person":
+                if confidence > 0.5:
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+                    outline.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))            
+               
+    box_line = cv2.dnn.NMSBoxes(outline, confidences, 0.5, 0.3)
+    if len(box_line) > 0:
+        flat_box = box_line.flatten()
+        pairs = []
+        center = []
+        status = [] 
+        for i in flat_box:
+            (x, y) = (outline[i][0], outline[i][1])
+            (w, h) = (outline[i][2], outline[i][3])
+            center.append([int(x + w / 2), int(y + h / 2)])
+            status.append(False)
+        #for every two objects check if close
+        for i in range(len(center)):
+            for j in range(len(center)):
+                close = Check_distance(center[i], center[j])
+                if close:
+                    pairs.append([center[i], center[j]])
+                    status[i] = True
+                    status[j] = True
+        index = 0
+        for i in flat_box:
+            (x, y) = (outline[i][0], outline[i][1])
+            (w, h) = (outline[i][2], outline[i][3])
+            #draw a rectangle around the person
+            #red if close
+            #green if not close
+            if status[index] == True:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 150), 2)
+            elif status[index] == False:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            index += 1
+        for h in pairs:
+            cv2.line(frame, tuple(h[0]), tuple(h[1]), (0, 0, 255), 2)
+    processedImg = frame.copy()
+    return processedImg
